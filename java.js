d013076be8f3c236ac1ed543f994ec6b6931ab8a
Sam <samromanhofman@gmail.com>
2019-06-19 21:17:23 +0200

    upload 4

---
+++ java.js
@@ -0,0 +1,18 @@
+/* Elementen selecteren */
+var picture2 = document.querySelector('.picture2');
+var button2 = document.querySelector('.button2');
+
+
+button2.addEventListener('click', function() {
+    picture2.classList.add('animatie');
+})
+
+var button3 = document.querySelector('.button3');
+var amazon = document.querySelector('.amazon');
+
+button3.addEventListener('click', function() {
+    amazon.classList.add('animatie2');
+})
+
+
+
